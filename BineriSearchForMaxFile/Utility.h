bcbaa01e74116ce040074d1e160104c8028c887a
Artak Avagyan <92084060+ArtakAvagyan@users.noreply.github.com>
2022-01-24 01:33:07 +0400

    Update Utility.h

--- BineriSearchForMaxFile/Utility.h
+++ BineriSearchForMaxFile/Utility.h
@@ -4,13 +4,14 @@
 
 #ifndef UTILITY_H
 #define UTILITY_H
+
 namespace Util
 {
 	const std::string path { "/home/art/C++/c++/BineriSearchForMaxFile/" };
 	const std::string fileName { "MaxFile.txt" };
 	const size_t maxCount { 4000000000 };
 	const size_t maxElemSize { UINT_MAX };
-	const size_t reng {1000};
+	const size_t reng { 10000 };
 	std::string tmpFileName {".a.txt"};
 
 } /* UTILITY_H */
